c0fa41ad46fcde08c956fca33e9bdcbf32b35fce
Михаел Йорданов <gaws@mail.bg>
2024-05-26 07:51:30 +0300

    chore: Update default route to "cities" in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import City from "./components/City";
@@ -38,7 +38,7 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>Dashboard</p>} />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
